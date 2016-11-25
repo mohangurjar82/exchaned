e1c14ea0ad9178eb06b94a3a1392a3689d381239
Exchanged app <mick_mccarthy@yahoo.com>
2016-11-25 21:24:14 +0000

    edit and show and update plus links

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,6 +4,22 @@ class ResourcesController < ApplicationController
    @resource = Resource.new
  end
   
+  
+  def edit
+  @resource =Resource.find(params[:id])
+  end
+  
+  
+  def update
+    @resource = Resource.find(params[:id])
+    if @resource.update(resource_params)
+      flash[:notice] = "Your Resource was successfully updated"
+      redirect_to resource_path(@resource)
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     @resource = Resource.new(resource_params)
     if @resource.save
@@ -17,6 +33,12 @@ class ResourcesController < ApplicationController
   def show
   @resource = Resource.find(params[:id])
   end
+  
+  
+  def index
+    @resources = Resource.all
+  end
+  
   
   private
   def resource_params
