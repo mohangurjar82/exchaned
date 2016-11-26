1d37fa977f14a0c82667e522c1fddcc9a5893a1e
Exchanged app <mick_mccarthy@yahoo.com>
2016-11-26 22:53:30 +0000

    Add password digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true, length: { maximum: 50 }, 
            uniqueness: { case_sensitive: false },
           format: { with: VALID_EMAIL_REGEX }
+has_secure_password
 
 end
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126215605) do
+ActiveRecord::Schema.define(version: 20161126224711) do
 
   create_table "rename_column_in_resources", force: :cascade do |t|
   end
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161126215605) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
