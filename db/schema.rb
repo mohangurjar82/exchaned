31bca051e215f790b10313367fec4ca82bb250d8
Exchanged app <mick_mccarthy@yahoo.com>
2016-11-26 21:18:28 +0000

    Update user model to branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124223018) do
+ActiveRecord::Schema.define(version: 20161126210421) do
 
   create_table "rename_column_in_resources", force: :cascade do |t|
   end
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20161124223018) do
     t.string   "name"
     t.text     "resource_type"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
